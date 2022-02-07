b50dbc4d8e0f9b26c47cfdfdd180b9790b65abd0
Martina Řádová <radovamar@natur.cuni.cz>
2022-02-07 16:55:49 +0100

    Dalsi vstupy pro uzivatele - polomer, zemepisna delka a sirka, vcetne osetreni vstupu

--- DU_05.py
+++ DU_05.py
@@ -6,14 +6,18 @@ La = Lambertovo azimutální
 Br = Braunovo válcové tečné
 Pt = Ptolemaiovo kuželové
 Sa = Sansonovo nepravé""")
-zobrazeni = input("Zadejte zkratku: ")
 # osetreni vstupu
-
-if zobrazeni in ["La", "Br", "Pt", "Sa"]:
-    print("Vybrali jste", zobrazeni)
-else:
-    ("Špatně zadané zobrazení. Zkuste to znovu")
-    zobrazeni = input("Zadejte zkratku: ")
+while True:
+    try:
+        zobrazeni = str(input("Zadejte zkratku: "))
+        if zobrazeni not in ["La", "Br", "Pt", "Sa"]:
+            print("Špatně zadané zobrazení. Zkuste to znovu")
+            continue
+        # pri korektnim vstupu vyskoci z cyklu
+        break
+    except ValueError:
+        print("Chybný vstup! Zadej znovu!")
+        continue
 
 # uzivatel zadava meritko
 print("""Zadejte měřítko, ve kterém bude zobrazení vytvořeno!
@@ -31,10 +35,57 @@ while True:
         print("Chybný vstup! Zadej znovu!")
         continue
 
+# uzivatel zadava polomer
+print("""Zadejte poloměr Země, se kterým chcete počítat. Zadejte celé číslo v km!
+Pokud zadáte 0, bude počítáno s poloměrem 6371,11 km.""")
+
+# osetreni spravneho vstupu a prevedeni polomeru na cm
+while True:
+    try:
+        polomer = float(input("Zadejte poloměr: "))
+        if polomer < 0:
+            print("Příliš malé číslo. Zkuste to znovu.")
+            continue
+        # pokud je polomer = 0, promenne je prirazena konstanta
+        elif polomer == 0:
+            R = 6371.11 * 100000
+        else:
+            R = polomer * 100000
+    except ValueError:
+        print("Chybný vstup! Zadej znovu!")
+        continue
+    break
+# vypocet daneho polomeru dle meritka a velikosti pixelu
+R = R/meritko/0.03
+#print(R)
+
+# uzivatel zadava zemepisnou sirku a delku bodu
+while True:
+    try:
+        d = float(input("Zadej zeměpisnou délku ve stupních: "))
+        s = float(input("Zadej zeměpisnou šířku ve stupních: "))
+        if abs(d) > 180:
+            print("Tento úhel nelze použít, zkus menší")
+            continue
+        if abs(s) > 90:
+            print("Tento úhel nelze použít, zkus menší")
+            continue
+    except ValueError:
+        print("Chybný vstup. Zadej celé číslo!")
+        continue
+    break
+
+# vypocet bodu a vykresleni site dle zadani uzivatele
 
 
-#INPUT - HODNOTY ZOBRAZENÍ - La, Br, Pt, Sa
-        #MĚŘÍTKO
+
+
+
+
+
+
+
+
+         
         #SOUŘADNICE BODŮ - ZEMĚPISNÁ DÉLKA A ŠÍŘKA
-        #POLOMĚR ZEMĚ
-
+      
